50155a269f480c1b9e1e38543ba185adae506ab0
Jasper <jrz3@protonmail.com>
2021-10-29 19:51:15 -0500

    Add warp fadescreep supression flag

--- src/field_screen_effect.c
+++ src/field_screen_effect.c
@@ -102,13 +102,20 @@ void FadeInFromBlack(void)
 void WarpFadeOutScreen(void)
 {
     u8 currentMapType = GetCurrentMapType();
-    switch (GetMapPairFadeToType(currentMapType, GetDestinationWarpMapHeader()->mapType))
+    if (!FlagGet(FLAG_REMOVE_WARP_FADE))
     {
-    case 0:
-        FadeScreen(FADE_TO_BLACK, 0);
-        break;
-    case 1:
-        FadeScreen(FADE_TO_WHITE, 0);
+        switch (GetMapPairFadeToType(currentMapType, GetDestinationWarpMapHeader()->mapType))
+        {
+        case 0:
+            FadeScreen(FADE_TO_BLACK, 0);
+            break;
+        case 1:
+            FadeScreen(FADE_TO_WHITE, 0);
+        }
+    }
+    else
+    {
+        FlagClear(FLAG_REMOVE_WARP_FADE);
     }
 }
 
